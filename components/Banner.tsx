8dd38b579f7d1bbd38ae3c45f34e88923145f6fe
Daniel <danielshi96@gmail.com>
2023-01-17 01:41:51 -0800

    objectFit deprecation adjust

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -24,7 +24,7 @@ function Banner({ horrorMovies }: Props) {
     return (
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
             <div className='absolute -z-10 top-0 left-0 h-[95vh] w-full'>
-                <Image src={`${baseUrl}/${movie?.backdrop_path || movie?.poster_path}`} object-fit='cover' alt={"random movie"} fill />
+                <Image src={`${baseUrl}/${movie?.backdrop_path || movie?.poster_path}`} fill style={{ objectFit: "cover" }} alt={"random movie"} />
             </div>
             <h1 className="text-2xl lg:text-7xl md:text-4xl font-bold">
                 {movie?.title || movie?.name || movie?.original_name}
@@ -32,8 +32,8 @@ function Banner({ horrorMovies }: Props) {
             <p className='text-shadow-md max-w-xs text-xs md:max-w-lg lg:max-w-2xl lg:text-2xl'>{movie?.overview}</p>
 
             <div className='flex space-x-3'>
-                <button className="bannerButton bg-white text-black"><FaPlay className="text-black md:h-7 md:w-7"/> Play</button>
-                <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/></button>
+                <button className="bannerButton bg-white text-black"><FaPlay className="text-black md:h-7 md:w-7" /> Play</button>
+                <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" /></button>
             </div>
         </div>
     )
